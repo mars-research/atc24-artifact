618ec52202205bbb537a621e44e45cfa2f53dc02
FeizaiYiHao <xiangdc@bu.edu>
2024-05-08 06:10:33 +0000

    add fig10 script

--- install_arm_clangs.sh
+++ install_arm_clangs.sh
@@ -6,3 +6,45 @@ git checkout arm_nacl
 nix develop --command bash -c  "cmake \$cmakeFlags -Sllvm -Bbuild -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_INSTALL_PREFIX="$PWD/../clang_install_dir/arm_nacl" && cd build/ && cmake --build . --target install -j5 && exit"
 cd ../
 fi
+
+if [ ! -d "$PWD/clang_install_dir/arm_c" ]; then
+cd llvm-SFI
+git checkout arm_c
+nix develop --command bash -c  "cmake \$cmakeFlags -Sllvm -Bbuild -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_INSTALL_PREFIX="$PWD/../clang_install_dir/arm_c" && cd build/ && cmake --build . --target install -j5 && exit"
+cd ../
+fi
+
+if [ ! -d "$PWD/clang_install_dir/arm_mte" ]; then
+cd llvm-SFI
+git checkout arm_mte
+nix develop --command bash -c  "cmake \$cmakeFlags -Sllvm -Bbuild -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_INSTALL_PREFIX="$PWD/../clang_install_dir/arm_mte" && cd build/ && cmake --build . --target install -j5 && exit"
+cd ../
+fi
+
+if [ ! -d "$PWD/clang_install_dir/arm_pac" ]; then
+cd llvm-SFI
+git checkout arm_pac
+nix develop --command bash -c  "cmake \$cmakeFlags -Sllvm -Bbuild -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_INSTALL_PREFIX="$PWD/../clang_install_dir/arm_pac" && cd build/ && cmake --build . --target install -j5 && exit"
+cd ../
+fi
+
+if [ ! -d "$PWD/clang_install_dir/arm_1reg" ]; then
+cd llvm-SFI
+git checkout arm_1reg
+nix develop --command bash -c  "cmake \$cmakeFlags -Sllvm -Bbuild -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_INSTALL_PREFIX="$PWD/../clang_install_dir/arm_1reg" && cd build/ && cmake --build . --target install -j5 && exit"
+cd ../
+fi
+
+if [ ! -d "$PWD/clang_install_dir/arm_ldst" ]; then
+cd llvm-SFI
+git checkout arm_ldst
+nix develop --command bash -c  "cmake \$cmakeFlags -Sllvm -Bbuild -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_INSTALL_PREFIX="$PWD/../clang_install_dir/arm_ldst" && cd build/ && cmake --build . --target install -j5 && exit"
+cd ../
+fi
+
+if [ ! -d "$PWD/clang_install_dir/arm_retcall" ]; then
+cd llvm-SFI
+git checkout arm_retcall
+nix develop --command bash -c  "cmake \$cmakeFlags -Sllvm -Bbuild -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_INSTALL_PREFIX="$PWD/../clang_install_dir/arm_retcall" && cd build/ && cmake --build . --target install -j5 && exit"
+cd ../
+fi
--- run_fig10.sh
+++ run_fig10.sh
@@ -3,7 +3,25 @@
 
 cd user-trampoline-arm64
 rm $PWD/../fig10.csv
+rm -f nfv_* *.o *.nosfi *.sfi 
 echo "ARM_C" >> $PWD/../fig10.csv
 git checkout atc_arm_c
 nix develop --command bash -c  "make && taskset -c 8 ./nfv_c 1 >> $PWD/../fig10.csv && taskset -c 8 ./nfv_c 4 >> $PWD/../fig10.csv && taskset -c 8 ./nfv_c 8 >> $PWD/../fig10.csv && taskset -c 8 ./nfv_c 16 >> $PWD/../fig10.csv && taskset -c 8 ./nfv_c 32 >> $PWD/../fig10.csv && exit"
+rm -f nfv_* *.o *.nosfi *.sfi 
+
+echo "ARM_NACL" >> $PWD/../fig10.csv
+git checkout atc_arm_nacl
+nix develop --command bash -c  "make && taskset -c 8 ./nfv_nacl 1 >> $PWD/../fig10.csv && taskset -c 8 ./nfv_nacl 4 >> $PWD/../fig10.csv && taskset -c 8 ./nfv_nacl 8 >> $PWD/../fig10.csv && taskset -c 8 ./nfv_nacl 16 >> $PWD/../fig10.csv && taskset -c 8 ./nfv_nacl 32 >> $PWD/../fig10.csv && exit"
+rm -f nfv_* *.o *.nosfi *.sfi 
+
+echo "ARM_MTE" >> $PWD/../fig10.csv
+git checkout atc_arm_mte
+nix develop --command bash -c  "make && taskset -c 8 ./nfv_mte 1 >> $PWD/../fig10.csv && taskset -c 8 ./nfv_mte 4 >> $PWD/../fig10.csv && taskset -c 8 ./nfv_mte 8 >> $PWD/../fig10.csv && taskset -c 8 ./nfv_mte 16 >> $PWD/../fig10.csv && taskset -c 8 ./nfv_mte 32 >> $PWD/../fig10.csv && exit"
+rm -f nfv_* *.o *.nosfi *.sfi 
+
+echo "ARM_PAC" >> $PWD/../fig10.csv
+git checkout atc_arm_pac
+nix develop --command bash -c  "make && taskset -c 8 ./nfv_pac 1 >> $PWD/../fig10.csv && taskset -c 8 ./nfv_pac 4 >> $PWD/../fig10.csv && taskset -c 8 ./nfv_pac 8 >> $PWD/../fig10.csv && taskset -c 8 ./nfv_pac 16 >> $PWD/../fig10.csv && taskset -c 8 ./nfv_pac 32 >> $PWD/../fig10.csv && exit"
+rm -f nfv_* *.o *.nosfi *.sfi 
+
 cd ..
