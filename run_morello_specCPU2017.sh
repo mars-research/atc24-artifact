5a896d66f424fd5f0e0fa9f08ab6a6dae006825b
FeizaiYiHao <xiangdc@bu.edu>
2024-05-11 06:13:07 +0000

    add script for 2017 x86 spec

--- run_morello_specCPU2017.sh
+++ run_morello_specCPU2017.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd spec-env
-nix develop --command bash -c "cd specCPU2017 && runcpu --config=morello_specCPU2017 --iterations=3 --threads=1 -copies=1 intspeed fpspeed && exit"
+nix develop --command bash -c "cd specCPU2017 && SPEC=\$PWD bin/runcpu --config=morello_specCPU2017 --iterations=3 --threads=1 -copies=1 intspeed fpspeed && exit"
 cd ..
